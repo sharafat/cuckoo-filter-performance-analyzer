63ebde7c1376760e8781712437f02cd17e76c026
Sharafat Ibn Mollah Mosharraf <sharafat_8271@yahoo.co.uk>
2018-09-14 23:44:38 +0600

    a23

--- src/main/java/bd/ac/buet/cse/ms/thesis/Key2EndpointMapper.java
+++ src/main/java/bd/ac/buet/cse/ms/thesis/Key2EndpointMapper.java
@@ -13,8 +13,11 @@ public class Key2EndpointMapper {
 
     private static final String[] imaginaryKeys = new String[] {
             "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+            "W", "X", "Y", "Z",
             "AA", "BB", "CC", "DD", "EE", "FF", "GG", "HH", "II", "JJ", "KK", "LL", "MM", "NN", "OO", "PP", "QQ", "RR",
-            "ABC", "DEF", "GHI", "JKL", "MNO", "PQR", "STU", "VWX", "YZ"
+            "SS", "TT", "UU", "VV", "WW", "XX", "YY", "ZZ",
+            "ABC", "DEF", "GHI", "JKL", "MNO", "PQR", "STU", "VWX", "YZ",
+            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"
     };
 
     public static void main(String[] args) {
